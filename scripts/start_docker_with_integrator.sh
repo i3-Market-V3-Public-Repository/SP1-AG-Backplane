658fb3c01cdd5ef535657369072a307cdf155d53
Marc Catrisse <marc.catrisse@upc.edu>
2022-08-30 11:30:47 +0200

    Refactor dockerfile, now uses buildkit + multistages

--- scripts/start_docker_with_integrator.sh
+++ scripts/start_docker_with_integrator.sh
@@ -29,15 +29,9 @@
 #  limitations under the License.
 #
 
-# If ./specs is not mapped or empty, clone from OAS Repo
-if [[ ! -d "specs" ]] || [[ $(ls -A "specs" | wc -l) -eq 0 ]] && [[ -n "$GITLAB_USER" ]] && [[ -n "$GITLAB_TOKEN" ]]; then
-  mkdir -p specs && \
-  git clone "https://$GITLAB_USER:$GITLAB_TOKEN@gitlab.com/i3-market/code/backplane/backplane-api-gateway/backplane-subsystems-oas" specs; \
-fi
-
-# Run integrator and update
-if [[ -f "/integrator/bulk_integrator" ]]; then
-  /integrator/bulk_integrator . specs && npm run build ;
+# If ./specs is mapped and not empty run integrator + npm run build
+if [[ -d "specs" ]] && [[ $(ls -A "specs" | wc -l) -gt 0 ]]; then
+  /integrator/bulk_integrator . specs && npm run build;
 fi
 
 node .
